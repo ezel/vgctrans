076ffd1ffc8c76ad6d28852a6bb905af05965a2c
stx1206 <martinshi1206@hotmail.com>
2024-10-08 13:50:52 +0800

    update css

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.module\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
